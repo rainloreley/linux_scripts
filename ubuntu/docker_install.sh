25a6afc6b1e5ff771fbcf6e4f1aeb294542da484
Loreley <mail@lory.dev>
2021-12-07 12:04:04 +0100

    added docker ubuntu

---
+++ ubuntu/docker_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# According to Dockers official website: https://docs.docker.com/engine/install/ubuntu/
+
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Setup repo
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release
+
+# add GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# setup stable repo
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+# install docker
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-compose
+
+# ===========
+echo "Done!"
