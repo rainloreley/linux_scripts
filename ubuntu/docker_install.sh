d0775700463817d37dc3dfb7f575177fe5e67df5
Loreley <mail@lory.dev>
2021-12-07 12:20:40 +0100

    fixed docker-compsoe

--- ubuntu/docker_install.sh
+++ ubuntu/docker_install.sh
@@ -20,7 +20,10 @@ echo \
 # install docker
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo apt-get install -y docker-compose
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # adding user to docker group
 sudo usermod -aG docker $USER
